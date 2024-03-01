ac35b52575d486f9e27ca384b19a80410453b61c
JonyM355 <joaoricardoam05@gmail.com>
2024-03-01 22:07:24 +0000

    Convert a string into a byte

---
+++ NomeDoProjeto/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NomeDoProjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Digite um número não-negativo: ");
+            string m = Console.ReadLine();
+            byte n = byte.Parse(m);
+            Console.WriteLine("Resultado da operação de decremento de n: ");
+            Console.WriteLine(--n);
+            Console.WriteLine("Resultado da operação de incremento de n: ");
+            Console.WriteLine(++n);
+
+        }
+    }
+}
