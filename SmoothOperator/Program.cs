b79eebce4ea08d92edfae20ab6861a9e6d6b6770
JonyM355 <joaoricardoam05@gmail.com>
2025-02-27 13:03:25 +0000

    Adding decrement and increment in a sbyte variable

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro:");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Decremento: {--n}");
+            Console.WriteLine($"Incremento: {++n}");
+        }
+    }
+}
