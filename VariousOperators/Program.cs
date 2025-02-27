4aacda1dd8b4f85f6d264bfa6747cada789688e4
JonyM355 <joaoricardoam05@gmail.com>
2025-02-27 14:04:57 +0000

    Doing operators for a sbyte variable

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número inteiro não negativo:");
+            sbyte n = sbyte.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{n / 2}");
+            Console.WriteLine($"{n << 3}");
+            Console.WriteLine($"{n ^ 6}");
+            Console.WriteLine($"{n > 10}");
+        }
+    }
+}
